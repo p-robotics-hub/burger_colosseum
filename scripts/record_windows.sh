b7bad1ba9d7bea26cf4d8e0fe7f41df1d85aca84
Hiroki Ikeuchi <ikeuchi.hiroki@jp.panasonic.com>
2021-01-22 17:58:51 +0900

    fix xid capturing command

--- scripts/record_windows.sh
+++ scripts/record_windows.sh
@@ -18,20 +18,14 @@ sudo chmod 777 ${TMP_DIR}
 
 WLIST=`wmctrl -l`
 
-XID_GAZEBO=`echo "$WLIST" | grep Gazebo | awk '{print $1}'`
-XID_JUDGE=`echo "$WLIST" | grep 'burger war' | awk '{print $1}'`
-XID_RVIZ=`echo "$WLIST" | grep 'RViz' | awk '{print $1}'`
+XID_GAZEBO=`echo "$WLIST" | grep Gazebo | awk '{print $1}'| head -1`
+XID_JUDGE=`echo "$WLIST" | grep 'burger war' | awk '{print $1}'| head -1`
+XID_RVIZ=`echo "$WLIST" | grep 'RViz' | awk '{print $1}'| head -1`
 
 
 (gst-launch-1.0 ximagesrc xid=$XID_GAZEBO ! queue ! video/x-raw,framerate=10/1 ! jpegenc ! avimux ! filesink location="${TMP_DIR}/01.avi")&
 (gst-launch-1.0 ximagesrc xid=$XID_JUDGE ! queue ! video/x-raw,framerate=1/1 ! jpegenc ! avimux ! filesink location="${TMP_DIR}/02.avi")&
 #(gst-launch-1.0 ximagesrc xid=$XID_RVIZ ! video/x-raw,framerate=10/1 ! jpegenc ! avimux ! filesink location="${TMP_DIR}/03.avi")&
-
-
-#(gst-launch-1.0 --eos-on-shutdown ximagesrc show-pointer=false use-damage=false xid=$XID_GAZEBO ! video/x-raw,framerate=10/1 ! videoconvert ! queue ! theoraenc ! mux. oggmux name=mux ! filesink location="${TMP_DIR}/01.ogv")&
-#(gst-launch-1.0 --eos-on-shutdown ximagesrc show-pointer=false use-damage=false xid=$XID_JUDGE ! video/x-raw,framerate=10/1 ! videoconvert ! queue ! theoraenc ! mux. oggmux name=mux ! filesink location="${TMP_DIR}/02.ogv")&
-#(gst-launch-1.0 --eos-on-shutdown ximagesrc show-pointer=false use-damage=false xid=$XID_RVIZ ! video/x-raw,framerate=10/1 ! videoconvert ! queue ! theoraenc ! mux. oggmux name=mux ! filesink location="${TMP_DIR}/03.ogv")&
-
 
 (gst-launch-1.0 ximagesrc ! queue ! video/x-raw,framerate=10/1 ! jpegenc ! avimux ! filesink location="${TMP_DIR}/00.avi")&
 
