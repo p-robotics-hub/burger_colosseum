69c1569ec22c749b6fa3862d834ebc7f2684e755
hiroki ikeuchi <ikeuchi.hirok@jp.panasonic.com>
2021-03-16 16:37:05 +0900

    fix

--- docker_robot_scripts/export_container_image.sh
+++ docker_robot_scripts/export_container_image.sh
@@ -7,12 +7,6 @@ echo CONTAINER_VER : $CONTAINER_VER
 
 set -x
 
-echo "Do you want to save the container image to local file?(y/n)"
-read IMAGE_YN
-if [ $IMAGE_YN = "y" ]; then
-	docker save burger-war-robo:${CONTAINER_VER} -o ~/burger-war-robo~${CONTAINER_VER}.tar
-fi
+docker save burger-war-robo:${CONTAINER_VER} > burger-war-robo~${CONTAINER_VER}.tar
 cd ~/
 tar cvfz catkin_ws_${CONTAINER_VER}.tgz catkin_ws_${CONTAINER_VER}
-
-set +x
